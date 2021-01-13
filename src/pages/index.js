7a2e7d74ee86236d0255fa9efa848de0948c48b8
Yanick <tyankee15@gmail.com>
2021-01-13 04:30:15 -0300

    Add aticles section in home page and in the articles page

--- src/pages/index.js
+++ src/pages/index.js
@@ -16,7 +16,7 @@ export default ({ data }) => {
     <Services />
     <Jobs />
     <Projects projects={projects} title="featured projects" showLink />
-    <Blogs blogs={blogs} title="article" showLink />
+    <Blogs blogs={blogs} title="latest articles" showLink />
   </Layout>
 }
 export const query = graphql`
