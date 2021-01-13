c5ab1a66e5ac09e9c1a02967738ff815b9f6e724
Yanick <tyankee15@gmail.com>
2021-01-13 03:48:02 -0300

    Add project section in home page and all projects in project page

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,11 +6,59 @@ import Services from "../components/Services"
 import Jobs from "../components/Jobs"
 import Projects from "../components/Projects"
 import Blogs from "../components/Blogs"
-export default () => {
+export default ({ data }) => {
+  const {
+    allStrapiProjects: { nodes: projects },
+    allStrapiBlogs: { nodes: blogs }
+  } = data
   return <Layout>
     <Hero />
     <Services />
     <Jobs />
+    <Projects projects={projects} title="featured projects" showLink />
+    <Blogs blogs={blogs} title="article" showLink />
   </Layout>
 }
-// ...GatsbyImageSharpFluid
+export const query = graphql`
+  {
+    allStrapiProjects(filter: {featured: {eq: true}}) {
+      nodes {
+        github
+        id
+        description
+        title
+        url
+        image {
+          childImageSharp {
+            fluid {
+              ...GatsbyImageSharpFluid
+            }
+          }
+        }
+        stack {
+          id
+          title
+        }
+      }
+    }
+
+    allStrapiBlogs(sort: {fields: date, order: DESC}, limit: 3) {
+      nodes {
+        slug
+        content
+        desc
+        date(formatString: "MMM Do, YYYY")
+        id
+        title
+        category
+        image {
+          childImageSharp {
+            fluid {
+              ...GatsbyImageSharpFluid
+            }
+          }
+        }
+      }
+    }
+  }
+`
